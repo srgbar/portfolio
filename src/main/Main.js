d459739c9cf742c207657dfd2a0fc95a6b8e96a3
srgbar <srg.bar@yandex.ru>
2021-12-15 00:39:19 +0300

    added page Skills

--- src/main/Main.js
+++ src/main/Main.js
@@ -1,18 +1,19 @@
 import React from "react";
 import style from "./Main.module.css";
+import styleContainer from "../common/styles/Container.module.css";
 import photo from "../images/photoSV.jpg"
 import ReactTypingEffect from "react-typing-effect";
 
 function Main() {
     return (
         <div className={style.mainBlock}>
-            <div className={style.container}>
+            <div className={styleContainer.container}>
                 <div className={style.description}>
                     <span>Hi There!</span>
                     <h1>I am <span className={style.surname}>Barbolin Sergey</span></h1>
-                    <ReactTypingEffect text={['Front-end developer who loves React and Information Technology']} eraseSpeed={0} typingDelay={1000}
+                    <ReactTypingEffect text={['Front-end developer who loves React and Information Technology']}
+                                       eraseSpeed={0} typingDelay={1000} speed={50}
                                        className={style.aboutMeFooterText}/>
-                    {/*<p>Front-end developer who loves React and Information Technology</p>*/}
                 </div>
                 <div className={style.photo}>
                     <img src={photo}/>
